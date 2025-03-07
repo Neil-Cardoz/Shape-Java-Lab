a587f13f5870987ebd86a3dfcd604851da58d545
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 20:50:14 +0530

    Defined the Parameterized Constructor

--- Shape.java
+++ Shape.java
@@ -6,9 +6,19 @@ abstract class Shape {
     protected double dim_three;
     protected int num_sides;
 
+    // Zero Parameters
     public Shape() {
         dim_one = dim_two = dim_three = 0.0;
         num_sides = 0;
+    }
+
+    //Parameterized Constructor
+    public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.num_sides = num_sides;
+
     }
 
 
