cca60968bf28cbea268067c912626351573668e2
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:01:31 +0530

    Added implementation to the methods of the Circle Class

--- Circle.java
+++ Circle.java
@@ -2,5 +2,18 @@
 
 import java.lang.*;
 class Circle extends Shape{
+    public Circle(double radius){
+        super(radius,0,0,0);
+    }
 
+
+    @Override
+    public double calculateArea() {
+        return  3.14 * (dim_one * dim_one);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return  2 * 3.14 *(dim_one);
+    }
 }
