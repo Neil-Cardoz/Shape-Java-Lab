1478074449e4792c72a1d75eb9e1e171003b19ef
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:14:49 +0530

    Update Rectangle.java
    Implemented the methods of Shape in Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,6 +1,20 @@
 // Rectangle.java
 
 class Rectangle extends Shape{
-	
+
+	public Rectangle(double length, double breadth){
+		super(length, breadth,0,4);
+	}
+
+	@Override
+	public double calculateArea(){
+		return dim_one * dim_two;
+	}
+
+
+	@Override
+	public double calculatePerimeter(){
+		return 2 * (dim_one + dim_two);
+	}
 
 }
