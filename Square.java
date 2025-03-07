11a1c72fe791388f17a8a8b94c57d131f6d9a8c9
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:03:40 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,15 @@
+// Square.java
+
+
+class Square extends Shape{
+
+	@Override
+	public double calculateArea() {
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		return 0;
+	}
+}
