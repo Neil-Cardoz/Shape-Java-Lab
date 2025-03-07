ac7a66b4e13ef9d689bfd49da4d69ea336bc4ee5
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:30:32 +0530

    Update Sphere.java
    Implemented the methods of Shape in Sphere along with implementation of VolumeInterface

--- Sphere.java
+++ Sphere.java
@@ -1,5 +1,23 @@
 // Sphere.java
 
 class Sphere extends Shape implements VolumeInterface{
-	
+	public Sphere(double radius){
+		super(radius,0,0,0);
+	}
+
+	@Override
+	public double calculateArea(){
+		return 4 * 3.14 * (dim_one * dim_one);
+	}
+
+	@Override
+	public double calculatePerimeter(){ // Same as Circle
+		return 2 * 3.14 *(dim_one);
+	}
+
+	@Override
+	public double calculateVolume(){
+		return (4.0 / 3.0)* 3.14 * ((dim_one) * (dim_one) *(dim_one));
+	}
+
 }
