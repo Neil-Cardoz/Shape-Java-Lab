480ae63eb66735147929c663a458f2f6ee0ba57c
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:12:33 +0530

    Implemented methods of Shape in Equilateral Pyramid
    Even added the VolumeInterface interface implementation

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,19 +1,25 @@
 // Equilateral Pyramid.java
 
-
+import java.lang.*;
 class EquilateralPyramid extends Shape implements VolumeInterface {
-	@Override
-	public double calculateArea() {
-		return 0;
+	public EquilateralPyramid(double side, double height){
+		super(side,height,0,0);
 	}
 
 	@Override
-	public double calculatePerimeter() {
-		return 0;
+	public double calculateArea(){
+		return dim_one * (dim_one + Math.sqrt(dim_one * dim_one + 4 * dim_two * dim_two));
 	}
 
+
 	@Override
-	public double calculateVolume() {
-		return 0;
-	}
+	public double calculatePerimeter(){
+	return 4 * (dim_one);
+}
+
+	@Override
+	public double calculateVolume(){
+	return (1.0 / 3.0) * (dim_one * dim_one) * dim_two;
+}
+
 }
