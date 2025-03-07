a992ba4b513d396b28afb5fbd16585014b5c8db8
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:15:58 +0530

    Update Cube.java
    Initialized the abstract methods and interface

--- Cube.java
+++ Cube.java
@@ -3,4 +3,18 @@
 
 class Cube extends Shape implements VolumeInterface{
 
+	@Override
+	public double calculateArea() {
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		return 0;
+	}
+
+	@Override
+	public double calculateVolume() {
+		return 0;
+	}
 }
