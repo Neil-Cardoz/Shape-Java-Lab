f20488e42547ea4bd4afb1585c4da20a728b217e
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:06:13 +0530

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+//Triangle.java
+
+class Triangle extends Shape{
+
+
+	@Override
+	public double calculateArea() {
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		return 0;
+	}
+}
