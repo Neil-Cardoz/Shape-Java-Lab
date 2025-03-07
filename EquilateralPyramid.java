d5b0b23557c5df5c3004f421cc4aec49afe94034
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-03-07 22:09:01 +0530

    Create EquilateralPyramid.java

---
+++ EquilateralPyramid.java
@@ -0,0 +1,19 @@
+// Equilateral Pyramid.java
+
+
+class EquilateralPyramid extends Shape implements VolumeInterface {
+	@Override
+	public double calculateArea() {
+		return 0;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		return 0;
+	}
+
+	@Override
+	public double calculateVolume() {
+		return 0;
+	}
+}
